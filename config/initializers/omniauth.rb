18eab467fff3212d6ed78d729f3a96c3b040b834
DavidPee <david_tahwei@hotmail.com>
2016-12-27 18:00:24 +0800

    Revert "facebook login added"

--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FB_APP_KEY'], ENV['FB_APP_SECRET']
-end
--- db/migrate/20161227090404_change_password_required_for_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangePasswordRequiredForUsers < ActiveRecord::Migration
-  def change
-  	change_column_null :users, :encrypted_password, true
-  end
-end
