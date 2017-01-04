49226cd58e0e21d762b36e2f0ea9a44463bc4967
David Pee <david_tahwei@hotmail.com>
2017-01-04 14:09:53 +0800

    gem 'carrierwave'gem 'mini_magick' installed

--- Gemfile
+++ Gemfile
@@ -37,6 +37,9 @@ gem 'filterrific'
 
 gem 'faker'
 
+gem 'carrierwave'
+gem 'mini_magick'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103035249) do
+ActiveRecord::Schema.define(version: 20170104052119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 20170103035249) do
     t.string   "remember_token",     limit: 128, null: false
     t.string   "username"
     t.string   "name"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
