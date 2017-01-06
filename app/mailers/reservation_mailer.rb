b8e12db8a731161d159309a0567a88f3c1bf75e1
David Pee <david_tahwei@hotmail.com>
2017-01-06 12:32:45 +0800

    Done with welcome email and reservation email gem install sidekiq

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,2 +1,9 @@
 class ReservationMailer < ApplicationMailer
+	#Send a confirmation email to user 
+	def reservation_email(user, listing)
+		@user = user
+		@listing = listing 
+
+		mail(to: @user.email, subject: 'Thank you for using Pair BnB')
+	end
 end
